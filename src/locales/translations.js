7cc3bb029b48cbe03a73e522e492c6d4458b13b7
ryo09020 <ryozinl924@gmail.com>
2025-12-22 14:21:27 +0900

    chore

--- src/locales/translations.js
+++ src/locales/translations.js
@@ -295,7 +295,6 @@ export const translations = {
           { label: 'ニュース', url: 'https://www.is.tohoku.ac.jp/jp/news/information/detail---id-5563.html' }
         ],
         image: '/images/ketchup.png',
-        imagePosition: 'top'
       },
       {
         title: 'Finger Drawing',
